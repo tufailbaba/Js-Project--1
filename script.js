93e7c129f55d0f9e3c75e8310db741b4cf33c7da
Tufail <tufailbabacoc@gmail.com>
2025-10-23 21:11:59 +0530

    Condtions added

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const prompt = require('prompt-sync')()
-const name  = prompt("What is your name? ")
-console.log("Hello", name ,"welcome to our game!");
-const shouldEePlay = prompt('Do you want to play?')
-
+const prompt = require("prompt-sync")();
+const name = prompt("What is your name? ");
+console.log("Hello", name, "welcome to our game!");
+const shouldWePlay = prompt("Do you want to play?");
+if (shouldWePlay.toLocaleLowerCase() === "yes") {
+  console.log("Okay we will play!");
+}
+else if(shouldWePlay.toLocaleLowerCase() === "yes"){
+ console.log("Okay :(");
+ 
+}
+else{
+    console.log("Invalid Input");
+    
+}
