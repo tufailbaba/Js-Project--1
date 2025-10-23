06159b9ef14b83cec64e42a2050d698ed82b102d
Tufail <tufailbabacoc@gmail.com>
2025-10-23 21:00:42 +0530

    Project-1

---
+++ script.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')()
+const name  = prompt("What is your name? ")
+console.log("Hello", name ,"welcome to our game!");
+const shouldEePlay = prompt('Do you want to play?')
+
