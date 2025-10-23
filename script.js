c3e1099216d3344a6552d33b21de91c550757469
Tufail <tufailbabacoc@gmail.com>
2025-10-23 21:27:00 +0530

    Left or Right -Game

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ const name = prompt("What is your name? ");
 console.log("Hello", name, "welcome to our game!");
 const shouldWePlay = prompt("Do you want to play?");
 if (shouldWePlay.toLocaleLowerCase() === "yes") {
-  console.log("Okay we will play!");
-}
-else if(shouldWePlay.toLocaleLowerCase() === "yes"){
- console.log("Okay :(");
- 
-}
-else{
-    console.log("Invalid Input");
-    
+  //Game logic
+  const leftOrRight = prompt(
+    "You enter a maze, do you want to go left or right?"
+  );
+  if (leftOrRight.toLocaleLowerCase ()=== "left") {
+    console.log("you go left and see a bridge.....");
+    const cross = prompt("Do you want to cross the bridge");
+    if (cross === "yes") {
+      console.log(
+        "You cross the but bridge was week and broke and you fell.. you lost."
+      );
+    } else {
+      console.log("Good chioce .. You win");
+    }
+  } else {
+    console.log("You go right and fall of a cliff...");
+  }
+} else if (shouldWePlay.toLocaleLowerCase() === "yes") {
+  console.log("Okay :(");
+} else {
+  console.log("Invalid input....");
 }
